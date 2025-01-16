b48d1999a8eeb476283027370790fabcb1d2b5e5
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2025-01-16 11:58:57 +0100

    16/01/25

--- f02-tabellbag-fyllinn/src/dat102/f02/tabellbag/TabellBag.java
+++ f02-tabellbag-fyllinn/src/dat102/f02/tabellbag/TabellBag.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * 
  * @author Lars-Petter Helland
  */
-public class TabellBag<T> {
+public class TabellBag<T> implements BagADT<T> {
 
 	private static final int DEFAULT_KAPASITET = 10;
 
@@ -24,6 +24,86 @@ public class TabellBag<T> {
 	public TabellBag(int kapasitet) {
 		tabell = (T[]) new Object[kapasitet];
 		antall = 0;
+	}
+
+	@Override
+	public int getCurrentSize() {
+		return antall;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return antall == 0;
+	}
+
+	private boolean erFull() {
+		return antall == tabell.length;
+	}
+
+	@Override
+	public boolean add(T newEntry) {
+
+		if (erFull()) {
+			return false;
+		}
+
+		tabell[antall] = newEntry;
+
+		antall++;
+
+		return true;
+	}
+
+	@Override
+	public T remove() {
+
+		if (isEmpty()) {
+			return null;
+		}
+
+		T temp = tabell[antall - 1];
+
+		tabell[antall - 1] = null;
+
+		antall--;
+
+		return temp;
+	}
+
+	@Override
+	public boolean remove(T anEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getFrequencyOf(T anEntry) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(T anEntry) {
+
+		for (int i = 0; i < antall; i++) {
+			if (tabell[i].equals(anEntry)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/************************************************************/
--- f02-tabellbag-fyllinn/src/dat102/f02/tabellbag/TabellBagTest.java
+++ f02-tabellbag-fyllinn/src/dat102/f02/tabellbag/TabellBagTest.java
@@ -8,7 +8,36 @@ class TabellBagTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+
+		BagADT<String> bag = new TabellBag<String>(2);
+
+		assertEquals(0, bag.getCurrentSize());
+		assertTrue(bag.isEmpty());
+
+		assertFalse(bag.contains("Per"));
+
+		assertTrue(bag.add("Per"));
+		assertTrue(bag.add("Bashar"));
+		assertFalse(bag.add("Alex"));
+
+		assertEquals(2, bag.getCurrentSize());
+		assertFalse(bag.isEmpty());
+
+		assertTrue(bag.contains("Per"));
+		assertTrue(bag.contains("Bashar"));
+		assertFalse(bag.contains("Alex"));
+
+		assertNotNull(bag.remove());
+		assertNotNull(bag.remove());
+		assertNull(bag.remove());
+
+		assertEquals(0, bag.getCurrentSize());
+		assertTrue(bag.isEmpty());
+
+		assertFalse(bag.contains("Per"));
+		assertFalse(bag.contains("Bashar"));
+		assertFalse(bag.contains("Alex"));
+
 	}
 
 }
